0983521003c874256ac66fe3fe4bb4659bf2aec0
niker.ling <niker.ling@shopper.com>
2022-07-14 16:26:29 +0800

    commit

--- app/Services/Socket.ts
+++ app/Services/Socket.ts
@@ -7,18 +7,41 @@ class SocketsController {
   async saveChat(data) {
     await SocketChat.create(data)
   }
-  async getMsgs() {
+  async saveUser(data) {
+    return await SocketUser.create(data)
+  }
+  async findUser(id) {
+    var data = await SocketUser.query().where('id', id)
+    var newData = data.map((item) => item.toJSON())
+    return newData;
+  }
+
+  async getMsgs(query) {
     // await SocketChat.create(data)
-    var data = await SocketChat.query().whereIn('from', ['1','2']).andWhereIn('to', ['1', '2']);
-    data = data.map((item) => item.toJSON())
+    console.log('query', query)
+    var newData = await SocketChat.query().whereIn('from', [query.from, query.to]).andWhereIn('to', [query.from, query.to]);
+    var data = newData.map((item) => item.toJSON())
     for(var i = 0; i < data.length; i++) {
-      if(data[i].from == '1'){
+      if(data[i].from == query.from){
         data[i].flow = 'out'
       } else {
         data[i].flow = 'in'
       }
     }
     return data;
+  }
+  async getFriend(data) {
+    // console.log(data)
+    return await SocketFriend.query().where('friend_1', data.account);
+  }
+
+  async addFriend(data) {
+    var res = await SocketFriend.query().where('friend_1', data.friend_1).andWhere('friend_2',data.friend_2);
+    if (res.length) {
+      return;
+    } else {
+      return await SocketFriend.create(data);
+    }
   }
 }
 
--- start/socket.ts
+++ start/socket.ts
@@ -8,26 +8,38 @@ var socketMap = {}
  * Listen for incoming socket connections
  */
 Ws.io.on('connection', (socket) => {
-  socket.on('login', (data) => {
-    socket.emit('loginFb', {
-      code: 200, 
-      msg: '登录成功',
-      friends: [
-        {
-          socket_nick: 'nick2',
-          socket_avatar: 'https://img1.baidu.com/it/u=2410334281,3176912851&fm=253&fmt=auto&app=138&f=JPEG?w=511&h=500',
-          unread: '0',
-          time: 1231,
-          lastMsg: {
-            from: '2',
-            to: '1',
+  socket.on('login', async(data) => {
+    var res = await SocketsController.findUser(data.account);
+    if (res.length) {
+      socket['account'] = data.account;
+      socketMap[socket.id] = socket;
+      socket.emit('loginFb', {
+        code: 200, 
+        msg: '登录成功',
+        socket_id: data.account + "_" + socket.id,
+        account : data.account,
+        friends: [
+          {
+            socket_nick: 'nick2',
+            socket_avatar: 'https://img1.baidu.com/it/u=2410334281,3176912851&fm=253&fmt=auto&app=138&f=JPEG?w=511&h=500',
+            unread: '0',
             time: 1231,
-            type: 'text',
-            text: '123456'
+            lastMsg: {
+              from: '2',
+              to: '1',
+              time: 1231,
+              type: 'text',
+              text: '123456'
+            }
           }
-        }
-      ]
-    })
+        ]
+      })
+    } else {
+      socket.emit('loginFb', {
+        code: 20001, 
+        msg: '没有账号',
+      })
+    }
   })
 
   socket.on('sayto', (data) => {
@@ -47,13 +59,69 @@ Ws.io.on('connection', (socket) => {
       flow: 'out'
     }
     // console.log('obj-', obj)
-    socket.emit('saytoFb', fbObj)
+    socket.emit('saytoFb', fbObj);
+
+    for (var key in socketMap) {
+      if(socketMap[key].account == data.to) {
+        fbObj.flow = 'in'
+        socketMap[key].emit('onmsg', fbObj)
+      }
+    }
+
     SocketsController.saveChat(obj)
   })
 
   socket.on('getMsgs', async(data) => {
-    var res = await SocketsController.getMsgs()
+    console.log("data----------------->", data, socket.account)
+    var query = {
+      from: socket['account'],
+      to: data.account
+    }
+    var res = await SocketsController.getMsgs(query)
     socket.emit('getMsgsFb', res)
   })
 
+  socket.on('getFriends', async(data)=>{
+    var res = await SocketsController.getFriend(data)
+    socket.emit('getFriendsFb', res)
+  })
+
+  socket.on('sginup', async(data)=>{
+    // pass: data.pass,
+    var obj = {
+      socket_nick: data.nick,
+      socket_avatar: "https://img1.baidu.com/it/u=2410334281,3176912851&fm=253&fmt=auto&app=138&f=JPEG?w=511&h=500",
+      level: '1',
+      time: '0',
+      ban: '1'
+    }
+    var res = await SocketsController.saveUser(obj)
+    console.log('res', res);
+    socket.emit('onsginup', res)
+  })
+
+  socket.on('finduser', async(data)=>{
+    var res = await SocketsController.findUser(data);
+    socket.emit('onfinduser', res)
+  })
+
+  socket.on('addfriend', async(data)=>{
+    var obj = {
+      'friend_1': data.id,
+      'friend_2': socket['account'],
+      'black': '2',
+      'blacklisted': '2',
+      'contact': '1',
+    }
+    var obj1 = {
+      'friend_1': socket['account'],
+      'friend_2': data.id,
+      'black': '2',
+      'blacklisted': '2',
+      'contact': '1',
+    }
+    await SocketsController.addFriend(obj);
+    await SocketsController.addFriend(obj1);
+    socket.emit('onaddfriend', { account: socket['account'] });
+  })
 })
